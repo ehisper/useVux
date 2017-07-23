80e76064b51999a8c977d4d56bf9b565817fcaa9
yangshenmeng <17717039902@163.com>
2017-07-23 12:08:33 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import { AlertPlugin, ToastPlugin } from 'vux'
-Vue.use(AlertPlugin)
-Vue.use(ToastPlugin)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import { AlertPlugin, ToastPlugin } from 'vux'
 
+Vue.use(AlertPlugin)
+Vue.use(ToastPlugin)
 Vue.use(Router)
 
 export default new Router({
